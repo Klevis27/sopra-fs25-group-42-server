4098dbb42b07e33e5fc2690399f489b110dede78
Necati Furkan Çolak <108272506+nfcolak@users.noreply.github.com>
2025-04-06 21:17:07 +0200

    Some message

--- src/main/java/ch/uzh/ifi/hase/soprafs24/exceptions/GlobalExceptionAdvice.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/exceptions/GlobalExceptionAdvice.java
@@ -11,10 +11,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @ControllerAdvice(annotations = RestController.class)
 public class GlobalExceptionAdvice extends ResponseEntityExceptionHandler {
@@ -24,20 +24,36 @@ public class GlobalExceptionAdvice extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = { IllegalArgumentException.class, IllegalStateException.class })
     protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
         log.error("Conflict Exception:", ex);
-        return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.CONFLICT, request);
+        return handleExceptionInternal(
+                ex,
+                Map.of("message", ex.getMessage()),
+                new HttpHeaders(),
+                HttpStatus.CONFLICT,
+                request
+        );
     }
 
     @ExceptionHandler(TransactionSystemException.class)
-    public ResponseStatusException handleTransactionSystemException(Exception ex, HttpServletRequest request) {
+    public ResponseEntity<Object> handleTransactionSystemException(Exception ex, HttpServletRequest request) {
         log.error("Request: {} raised {}", request.getRequestURL(), ex);
-        return new ResponseStatusException(HttpStatus.CONFLICT, ex.getMessage(), ex);
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(Map.of("message", ex.getMessage()));
     }
 
-    // Keep this one disable for all testing purposes -> it shows more detail with
-    // this one disabled
     @ExceptionHandler(HttpServerErrorException.InternalServerError.class)
-    public ResponseStatusException handleException(Exception ex) {
+    public ResponseEntity<Object> handleInternalServerError(Exception ex) {
         log.error("Default Exception Handler -> caught:", ex);
-        return new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), ex);
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Map.of("message", ex.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleAll(Exception ex) {
+        log.error("Unhandled exception caught:", ex);
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("message", ex.getMessage()));
     }
 }
---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/VaultDTO.java
@@ -0,0 +1,17 @@
+package ch.uzh.ifi.hase.soprafs24.rest.dto;
+
+import ch.uzh.ifi.hase.soprafs24.entity.Vault;
+
+public class VaultDTO {
+    public Long id;
+    public String name;
+    public String state;
+
+    public static VaultDTO fromEntity(Vault vault) {
+        VaultDTO dto = new VaultDTO();
+        dto.id = vault.getId();
+        dto.name = vault.getName();
+        dto.state = vault.getState();
+        return dto;
+    }
+}
