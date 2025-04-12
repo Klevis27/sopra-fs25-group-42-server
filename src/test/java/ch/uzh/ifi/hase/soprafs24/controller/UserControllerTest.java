160bb528383850a4d3e664fc40134b5f089d41c8
oliverbenjamin.hostettler <oliverbenjamin.hostettler@uzh.ch>
2025-04-12 11:55:04 +0200

    Test for getting user dashboard with invalid token

--- src/test/java/ch/uzh/ifi/hase/soprafs24/controller/UserControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/controller/UserControllerTest.java
@@ -256,8 +256,47 @@ public class UserControllerTest {
                                 .content(asJsonString(userLoginDTO));
 
                 mockMvc.perform(postRequest)
-                                .andDo(print()) 
+                                .andDo(print())
                                 .andExpect(status().isUnauthorized());
+        }
+
+        // Test how GET "/users" handles a request with valid input and if it gives back
+        // 200 OK
+        @Test
+        public void getUserDashboard_validInput_Ok() throws Exception {
+
+                User user = new User();
+                user.setId(1L);
+
+                given(userRepository.findById(1L)).willReturn(Optional.of(user));
+                given(jwtUtil.extractId(Mockito.anyString())).willReturn("1");
+                given(jwtUtil.validateToken(Mockito.anyString(), Mockito.eq("1"))).willReturn(true);
+
+                MockHttpServletRequestBuilder getRequest = get("/users")
+                                .header("Authorization", "Bearer validToken")
+                                .contentType(MediaType.APPLICATION_JSON);
+
+                mockMvc.perform(getRequest)
+                                .andExpect((status().isOk()));
+        }
+
+        // Test how GET "/users" handles a request with valid input but invalid token 
+        // and gives back 401 UNAUTHORIZED
+        @Test
+        public void getUserDashboard_invalidInput_Unauthorized() throws Exception {
+
+                User user = new User();
+                user.setId(1L);
+
+                given(jwtUtil.extractId(Mockito.anyString())).willReturn("1");
+                given(jwtUtil.validateToken(Mockito.anyString(), Mockito.eq("1"))).willReturn(false);
+
+                MockHttpServletRequestBuilder getRequest = get("/users")
+                                .header("Authorization", "Bearer validToken")
+                                .contentType(MediaType.APPLICATION_JSON);
+
+                mockMvc.perform(getRequest)
+                                .andExpect((status().isUnauthorized()));
         }
 
 }
