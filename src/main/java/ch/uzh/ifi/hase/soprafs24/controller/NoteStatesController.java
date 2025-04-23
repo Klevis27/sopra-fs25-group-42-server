dc8cb537fa2eccf43e83c62ccff1e73fd8473c93
oliverbenjamin.hostettler <oliverbenjamin.hostettler@uzh.ch>
2025-04-23 11:26:59 +0200

    Creating and updating notestates works from postman

--- src/main/java/ch/uzh/ifi/hase/soprafs24/controller/NoteStatesController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/controller/NoteStatesController.java
@@ -1,5 +1,6 @@
 package ch.uzh.ifi.hase.soprafs24.controller;
 
+import ch.uzh.ifi.hase.soprafs24.rest.dto.NoteStatePostDTO;
 import ch.uzh.ifi.hase.soprafs24.rest.dto.NoteStatePutDTO;
 import ch.uzh.ifi.hase.soprafs24.service.NoteStateService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +16,7 @@ public class NoteStatesController {
         this.noteStateService = noteStateService;
     }
 
-    @PutMapping("/noteState")
+    @PutMapping("/noteState/{note_Id}")
     public ResponseEntity<?> updateNoteStateContent(
             @PathVariable("note_Id") Long noteId, 
             @RequestBody NoteStatePutDTO noteStatePutDTO) {
@@ -35,4 +36,27 @@ public class NoteStatesController {
             return ResponseEntity.status(404).body("Note state not found");
         }
     }
+
+    @PostMapping("/noteState/{note_Id}")
+    public ResponseEntity<?> createNoteState(
+        @PathVariable("note_Id") Long noteId, 
+        @RequestBody NoteStatePostDTO noteStatePostDTO){
+
+                    // Step 1: Check if the provided noteId in the DTO matches the URL noteId
+        if (!noteId.equals(noteStatePostDTO.getNoteId())) {
+            return ResponseEntity.badRequest().body("Note ID in URL does not match the one in the body");
+        }
+
+        // Step 2: Delegate to the service to update the NoteState content
+        boolean isUpdated = noteStateService.createNoteState(noteStatePostDTO);
+
+        // Step 3: Return appropriate response based on success or failure
+        if (isUpdated) {
+            return ResponseEntity.ok("Note state created successfully");
+        } else {
+            return ResponseEntity.status(404).body("Note state could not be created");
+        }
+
+
+        }
 }
---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/rest/dto/NoteStatePostDTO.java
@@ -0,0 +1,32 @@
+package ch.uzh.ifi.hase.soprafs24.rest.dto;
+
+public class NoteStatePostDTO {
+    private Long documentId;
+
+    private Long noteId;
+    
+    private byte[] content;
+
+    public Long getDocId(){
+        return this.documentId;
+    }
+
+    public void setDocId(Long documentId){
+        this.documentId = documentId;
+    }
+
+    public Long getNoteId(){
+        return this.noteId;
+    }
+
+    public void setNote(Long noteId){
+        this.noteId = noteId;
+    }
+
+    public byte[] getContent(){
+        return this.content;
+    }
+    public void setContent(byte[] content){
+        this.content = content;
+    }
+}
