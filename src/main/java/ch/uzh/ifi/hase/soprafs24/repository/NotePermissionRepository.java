85de02e657c5c5ced841eacdeca571cb49c9a771
elifdenizi <elifdeniz.isbuga@uzh.ch>
2025-05-09 17:28:10 +0200

    backend part of the shared notes

--- src/main/java/ch/uzh/ifi/hase/soprafs24/repository/NotePermissionRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/repository/NotePermissionRepository.java
@@ -3,6 +3,8 @@ package ch.uzh.ifi.hase.soprafs24.repository;
 import ch.uzh.ifi.hase.soprafs24.entity.Note;
 import ch.uzh.ifi.hase.soprafs24.entity.NotePermission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,6 +19,11 @@ public interface NotePermissionRepository extends JpaRepository<NotePermission, 
     List<NotePermission> findByUserId(Long userId);
     
     void deleteAllByNote(Note note);
+
+    @Query("SELECT np.note FROM NotePermission np WHERE np.userId = :userId AND np.role <> 'OWNER'")
+    List<Note> findSharedNotesByUserId(@Param("userId") Long userId);
+    
+
 
     
 }
